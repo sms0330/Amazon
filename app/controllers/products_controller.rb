c482a7658dd75c004ba3c6a58e8097fc4e4e6368
Joseph Son <sms0330@gmail.com>
2021-07-08 20:30:56 -0700

    I did refactoring code v0.3.1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+    before_action :find_product, only: [:show, :edit, :update, :destroy]
     def index
         @products = Product.all.order(created_at: :desc) 
     end
@@ -7,33 +8,29 @@ class ProductsController < ApplicationController
         @product = Product.new
     end
 
-def create
+    def create
         @product = Product.new product_params
         if @product.save
-            redirect_to product_path(@product.id)
+            redirect_to product_path(@product), notice: "Product created successfully"
         else
             render :new
         end
     end
     
     def show
-        @product = Product.find(params[:id])
     end
     
     def destroy
-        @product = Product.find(params[:id])
         @product.destroy
         redirect_to products_path
     end
 
     def edit
-        @product = Product.find(params[:id])
     end
 
     def update
-        @product =  Product.find(params[:id])
         if @product.update product_params
-            redirect_to product_path(@product.id)
+            redirect_to product_path(@product)
         else
             render :edit
         end
@@ -42,6 +39,10 @@ def create
     
     
     private
+    def find_product
+        @product = Product.find params[:id]
+    end
+
     def product_params
         params.require(:product).permit(:title,:description,:price)
     end
